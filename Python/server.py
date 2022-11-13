382c276de3a7b587add5f75b234cd9f01733d835
Prajwal Nautiyal <prajwalnautiyal2003@gmail.com>
2022-11-12 20:56:15 -0500

    Updated the usage instructions for the server.py file

--- Python/server.py
+++ Python/server.py
@@ -2,6 +2,9 @@
 Dev: Prajwal Nautiyal
 Last Update: 11 November 2022
 A server that listens for incoming connections and prints the data received and stores the transmission info in a file.
+To test this, run the server.py file and then run the client.py file. 
+Alternatively, you can use the Putty client to connect to the server. 
+Use the RAW configuration (In the putty session window, choose Connection Type: Other and choose Raw from the drop down menu) with the IP and PORT of the server.
 """
 
 import csv                  # Importing the csv module to wrtite the data to a csv file
@@ -16,17 +19,16 @@ def serverConec():
     server = socket.create_server((host, port), family=socket.AF_INET)                                                     # Create a socket object                         # Bind to the port
     server.listen(10)                                   # Now wait for client connection.
     cli, addr = server.accept()                         # Establish connection with client.
+    cli.send('Thank you for connecting'.encode())       # Send a thank you message to the client.
     protocol = cli.proto                                # Extracting the protocol property from the connection object
     connection = str(cli)                               # Converting the connection object to a string
     src = re.findall("raddr=\('(\d{3}\.\d\.\d\.\d)\',\s(\d+)", connection)                                                  # Extracting the source IP and port from the connection string
     dest = re.findall("laddr=\('(\d{3}\.\d\.\d\.\d)\',\s(\d+)", connection)                                                 # Extracting the destination IP and port from the connection string
-    print(f'Got connection from {addr[0]} onto port {addr[1]}\nThe protocol is {protocol}')                                 # Print the address of the client
-    
+    print(f'Got connection from {addr[0]} onto port {addr[1]}\nThe protocol is {protocol}')                                 # Print the address of the client and the protocol used
     try:
         print(cli.recv(1234).decode())                  # Print the data received from the client
     except:
         pass
-    cli.send('Thank you for connecting'.encode())       # Send a thank you message to the client.
     cli.close()                                         # Close the connection
     with open('transmission.csv', mode='a') as fl:      # Open the file in write mode
         writer = csv.writer(fl, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)                                    # Create a csv writer object
@@ -41,13 +43,3 @@ if __name__ == '__main__':
     fl.close()                                          # Close the file
     while True:
         serverConec()
-        
-"""
-Citations
-1: https://docs.python.org/3/library/socket.html
-2: https://docs.python.org/3/library/csv.html
-3: https://www.tutorialspoint.com/python/python_files_io.htm
-4: https://www.geeksforgeeks.org/python-script-to-monitor-network-connection-and-saving-into-log-file/
-5: https://www.digitalocean.com/community/tutorials/python-socket-programming-server-client
-6: https://realpython.com/python-csv/
-"""

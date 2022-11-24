4f6b0c7bf3c09631d8a0f83bcd1ffbd5c087c1ab
Prajwal Nautiyal <prajwalnautiyal2003@gmail.com>
2022-11-23 21:31:19 -0500

    Fixed the issue with exception handling

--- Java/Abc.java
+++ Java/Abc.java
@@ -4,46 +4,88 @@
 * A Basic Calculator
 */
 
-import java.util.*;                                                 // Importing the java.util package to use the Scanner class
+// Importing the java.util package to use the Scanner class
+import java.util.Scanner;
 
 class Abc {                                                         // Declaring a class named Abc
+    static Scanner sc = new Scanner(System.in);                     // Creating a Scanner object
+    public static void main(String args[]) {
+        do {                                                        // Infinite loop to keep the program running until the user presses Ctrl+C
+            try {
+                double out[] = new double[3];
+                out = Abc.calc();                                   // Calling the calc function and storing the returned value in the variable 'out'
+                Abc.output(out[0], out[1], (int) out[2], out[3]);   // Calling the output function
+                System.out.println("Press Ctrl+C to exit");         // Printing a message to the user to press Ctrl+C to exit the program
+            } 
+            catch (Exception e) {
+                if (e instanceof java.util.InputMismatchException) { // Checking if the exception is of type InputMismatchException
+                    System.out.println("Invalid Input");            // Printing a message to the user that the input is invalid
+                } 
+                else if (e instanceof java.lang.ArithmeticException) { // Checking if the exception is of type ArithmeticException
+                    System.out.println("Cannot divide by zero");    // Printing a message to the user that the input is invalid
+                } 
+                else if (e instanceof java.util.NoSuchElementException){
+                    System.out.println("Program Terminated");       // Printing an exit message
+                }
+            }
+        }while (true);                                              // Infinite loop 
+    }
 
     static int[] input() {                                          // This function will take input from the user and return an array of integers
-        int arr[] = new int[3];                                     // Declaring an array of integers with size 3 to store the input
-        try (Scanner sc = new Scanner(System.in)) {                 // This try-with-resources block will automatically close the Scanner object after the code inside the block is executed
-            System.out.println("Enter first value:");
-            arr[0] = sc.nextInt();                                  // Taking input from the user for the first element
-            System.out.println("Enter second value:");
-            arr[1] = sc.nextInt();                                  // Taking input from the user for the second element
-            System.out.println("Choose,\n1 for Addition (+)\n2 for Substraction (-)\n3 for Multiplication (*)\n4 for Division (/)");
-            arr[2] = sc.nextInt();                                  // Taking input from the user for the operator
-        }
+        int arr[] = new int[2];                                     // Declaring an array of integers with size 3 to store the input
+        System.out.println("Enter first value:");
+        arr[0] = sc.nextInt();                                      // Taking input from the user for the first element
+        System.out.println("Enter second value:");
+        arr[1] = sc.nextInt();                                      // Taking input from the user for the second element
         return arr;                                                 // Returning the array
     }
 
-    static float calc(int a, int b, int c) {                        // This function will perform the calculation and return the result; 'a' is the first value, 'b' is the second value and 'c' is the operator choice
-        float val;                                                  // Declaring a variable to store the result
+    static double[] calc() {                                        // This function will perform the calculation and return the result; 'a' is the first value, 'b' is the second value and 'c' is the operator choice
+        System.out.println("Choose,\n1 for Addition (+)\n2 for Substraction (-)\n3 for Multiplication (*)\n4 for Division (/)\n5 for Modulus (%)\n6 for Power (^)\n7 to Exit");
+        int c = sc.nextInt();                                       // Taking input from the user for the operator choice
+        int arr[] = new int[2];                                     // Declaring an array of integers with size 2 to store the input
+        double out[] = new double[4];                               // Declaring an array of integers with size 4 to store the output
+        double val;                                                 // Declaring a variable to store the result
         switch (c) {                                                // Switch case to perform the calculation as per the user's choice of operator from the menu
             case 1:                                                 // Addition
-                val = a + b;
+                arr = input();                                        
+                val = arr[0] + arr[1];
                 break;
             case 2:                                                 // Substraction
-                val = a - b;
+                arr = input();                                        
+                val = arr[0] - arr[1];
                 break;
             case 3:                                                 // Multiplication
-                val = a * b;
+                arr = input();
+                val = arr[0] * arr[1];
                 break;
             case 4:                                                 // Division
-                val = a / b;
+                arr = input();
+                val = arr[0] / arr[1];
                 break;
+            case 5:                                                 // Modulus
+                arr = input();
+                val = arr[0] % arr[1];
+                break;
+            case 6:                                                 // Power
+                arr = input();
+                val = (Double) Math.pow(arr[0], arr[1]);
+                break;
+            case 7:                                                 // Exit
+                System.out.println("Program Terminated");            // Printing an exit message
+                System.exit(0);                                     // Exiting the program
             default:                                                // If the user enters a choice other than 1, 2, 3 or 4
                 val = 0;
                 break;
         }
-        return val;
+        out[0] = arr[0];
+        out[1] = arr[1];
+        out[2] = c;
+        out[3] = val;
+        return out;                                                 // Returning the result
     }
 
-    static void output(int a, int b, int op, float res) {           // This function will print the approppriate result depending on the operator 'op',  'a' and 'b' are the user inputted values and 'res' is the result
+    static void output(double a, double b, int op, double res) {    // This function will print the approppriate result depending on the operator 'op',  'a' and 'b' are the user inputted values and 'res' is the result
         switch (op) {                                               // Switch case to print the result as per the user's choice of operand from the menu
             case 1:                                                 // Output for addition
                 System.out.println("The sum of " + a + " and " + b + " is " + res);
@@ -57,24 +99,15 @@ class Abc {                                                         // Declaring
             case 4:                                                 // Output for division
                 System.out.println("The result of dividing " + a + " and " + b + " is " + res);
                 break;
+            case 5:                                                 // Output for modulus
+                System.out.println("The remainder when " + a + " is divided by " + b + " is " + res);
+                break;
+            case 6:                                                 // Output for power
+                System.out.println(a + " raised to the power " + b + " is " + res);
+                break;
             default:                                                // If the user enters a choice other than 1, 2, 3 or 4
                 System.out.println("Invalid choice");
                 break;
-        }
-    }
-
-    public static void main(String args[]) {
-        try {                                                       // This try block will execute the code inside it and if the keyboard interrupt exception is thrown, the catch block will execute
-            while (true) {                                          // Infinite loop to keep the program running until the user presses Ctrl+C
-                int ar[] = new int[3];                              // Declaring an array of integers with size 3 to store the input
-                ar = Abc.input();                                   // Calling the input function and storing the returned array in the array 'ar'
-                float out = Abc.calc(ar[0], ar[1], ar[2]);          // Calling the calc function and storing the returned value in the variable 'out'
-                Abc.output(ar[0], ar[1], ar[2], out);               // Calling the output function
-                System.out.println("Exit: Ctrl+C.");                // Printing the message to exit the program
-            }
-        } 
-        catch (Exception keyBoardException) {                       // This catch block will execute if the keyboard interrupt exception is thrown
-            System.out.println("Program Closing.");                 //Exit message
         }
     }
 }
